427b856c2d16d0c2c11564c25a8ecf0f9b8bfe49
Max Hilgers <hilgers.max@gmail.com>
2022-01-09 12:18:10 +0100

    minor bug fix

--- src/hilgers/max/Main.java
+++ src/hilgers/max/Main.java
@@ -3,10 +3,10 @@ package hilgers.max;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-	int a = 7;
-	int b = 5;
-	int result = a + b;
-	System.out.println(result);
+        // write your code here
+        int a = 7;
+        int b = 5;
+        int result = a + b;
+        System.out.println(result);
     }
 }
